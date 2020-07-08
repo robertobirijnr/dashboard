bf2fb5eb3b3e37f025096e5508ca3a75695adfd4
robertobirijnr <robertobirijnr@hotmail.com>
2020-07-08 14:57:26 +0000

    departments

--- src/components/Admin/Departments.vue
+++ src/components/Admin/Departments.vue
@@ -1,5 +1,13 @@
 <template>
     <div  class="main-content-container container-fluid px-4">
+      <!-- Page Header -->
+    <div class="page-header row no-gutters py-4">
+      <div class="col-12 col-sm-4 text-center text-sm-left mb-0">
+        <span class="text-uppercase page-subtitle">Overview</span>
+        <h3 class="page-title">Departmentss</h3>
+      </div>
+    </div>
+
         <div class="col">
           <div class="card card-small mb-4">
             <table class="table">
