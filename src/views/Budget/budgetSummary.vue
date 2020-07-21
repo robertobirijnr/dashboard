f96a32edb014c7ac51dafc756ddd7117ec77f88c
robertobirijnr <robertobirijnr@hotmail.com>
2020-07-21 08:00:25 +0000

    budget summary clean up

--- src/views/Budget/budgetSummary.vue
+++ src/views/Budget/budgetSummary.vue
@@ -13,114 +13,113 @@
             <div class="card-header">
               <button class="btn-sm btn btn-primary">EXCEL</button>
               <!--<ul class="nav nav-tabs" id="myTab" role="tablist">-->
-                <!--<li class="nav-item">-->
-                  <!--<a class="nav-link active" id="division-tab" data-toggle="tab" href="#division" role="tab" aria-controls="division" aria-selected="true">Division Budget Summary</a>-->
-                <!--</li>-->
-                <!--<li class="nav-item">-->
-                  <!--<a class="nav-link" id="department-tab" data-toggle="tab" href="#department" role="tab" aria-controls="department" aria-selected="false">Department budget Summary</a>-->
-                <!--</li>-->
-                <!--<li class="nav-item">-->
-                  <!--<a class="nav-link" id="unit-tab" data-toggle="tab" href="#unit" role="tab" aria-controls="unit" aria-selected="false">Unit Budget Summary</a>-->
-                <!--</li>-->
+              <!--<li class="nav-item">-->
+              <!--<a class="nav-link active" id="division-tab" data-toggle="tab" href="#division" role="tab" aria-controls="division" aria-selected="true">Division Budget Summary</a>-->
+              <!--</li>-->
+              <!--<li class="nav-item">-->
+              <!--<a class="nav-link" id="department-tab" data-toggle="tab" href="#department" role="tab" aria-controls="department" aria-selected="false">Department budget Summary</a>-->
+              <!--</li>-->
+              <!--<li class="nav-item">-->
+              <!--<a class="nav-link" id="unit-tab" data-toggle="tab" href="#unit" role="tab" aria-controls="unit" aria-selected="false">Unit Budget Summary</a>-->
+              <!--</li>-->
               <!--</ul>-->
             </div>
             <div class="card-body">
               <div class="table-responsive">
                 <table class="table table-sm table-bordered">
                   <thead>
-                  <tr align="center">
-                    <th :colspan="ubc">{{object.start_date}} - {{object.end_date}} Budget Summary</th>
-                  </tr>
-                  <tr>
-                    <th>Item</th>
-                    <th :key="ub.id" v-for="ub in budgets">{{ub.unit_name}}</th>
-                    <th>Totals</th>
-                    <!--<th>Unit C</th>-->
-                    <!--<th>Unit D</th>-->
-                  </tr>
+                    <tr align="center">
+                      <th :colspan="ubc">{{object.start_date}} - {{object.end_date}} Budget Summary</th>
+                    </tr>
+                    <tr>
+                      <th>Item</th>
+                      <th :key="ub.id" v-for="ub in budgets">{{ub.unit_name}}</th>
+                      <th>Totals</th>
+                      <!--<th>Unit C</th>-->
+                      <!--<th>Unit D</th>-->
+                    </tr>
                   </thead>
                   <tbody>
-                  <tr>
-                    <th :colspan="ubc">Compensation Summary</th>
-                  </tr>
-                  <tr>
-                    <td>Consolidated Basic</td>
-                    <td :key="ub.id" v-for="ub in budgets">GHS {{ub.consolidated_basic_salary}}</td>
-                    <td>erwr</td>
-                  </tr>
-                  <tr>
-                    <td>Allowances</td>
-                    <td :key="ub.id" v-for="ub in budgets">GHS {{ub.allowances}}</td>
-                    <td>fsfs</td>
-                  </tr>
-                  <tr>
-                    <th>Sub Total</th>
-                    <td :key="ub.id" v-for="ub in budgets">GHS {{ub.employees_compensation_total}}</td>
-                    <td>ffdf</td>
-                  </tr>
-                  <tr>
-                    <th :colspan="ubc">Goods & Services</th>
-                  </tr>
-                  <!--<tr>-->
+                    <tr>
+                      <th :colspan="ubc">Compensation Summary</th>
+                    </tr>
+                    <tr>
+                      <td>Consolidated Basic</td>
+                      <td :key="ub.id" v-for="ub in budgets">GHS {{ub.consolidated_basic_salary}}</td>
+                      <td>erwr</td>
+                    </tr>
+                    <tr>
+                      <td>Allowances</td>
+                      <td :key="ub.id" v-for="ub in budgets">GHS {{ub.allowances}}</td>
+                      <td>fsfs</td>
+                    </tr>
+                    <tr>
+                      <th>Sub Total</th>
+                      <td :key="ub.id" v-for="ub in budgets">GHS {{ub.employees_compensation_total}}</td>
+                      <td>ffdf</td>
+                    </tr>
+                    <tr>
+                      <th :colspan="ubc">Goods & Services</th>
+                    </tr>
+                    <!--<tr>-->
                     <!--<td>Travel and Transport</td>-->
                     <!--<td></td>-->
                     <!--<td></td>-->
                     <!--<td></td>-->
                     <!--<td></td>-->
-                  <!--</tr>-->
-                  <!--<tr>-->
+                    <!--</tr>-->
+                    <!--<tr>-->
                     <!--<td>General Cleaning</td>-->
                     <!--<td></td>-->
                     <!--<td></td>-->
                     <!--<td></td>-->
                     <!--<td></td>-->
-                  <!--</tr>-->
-                  <tr>
-                    <th>Sub Total</th>
-                    <td :key="ub.id" v-for="ub in budgets">GHS {{ub.goods_services_total}}</td>
-                    <td>werw</td>
-                  </tr>
-                  <tr>
-                    <th :colspan="ubc">Assets</th>
-                  </tr>
-                  <!--<tr>-->
+                    <!--</tr>-->
+                    <tr>
+                      <th>Sub Total</th>
+                      <td :key="ub.id" v-for="ub in budgets">GHS {{ub.goods_services_total}}</td>
+                      <td>werw</td>
+                    </tr>
+                    <tr>
+                      <th :colspan="ubc">Assets</th>
+                    </tr>
+                    <!--<tr>-->
                     <!--<td>Land</td>-->
                     <!--<td></td>-->
                     <!--<td></td>-->
                     <!--<td></td>-->
                     <!--<td></td>-->
-                  <!--</tr>-->
-                  <!--<tr>-->
+                    <!--</tr>-->
+                    <!--<tr>-->
                     <!--<td>Office Building</td>-->
                     <!--<td></td>-->
                     <!--<td></td>-->
                     <!--<td></td>-->
                     <!--<td></td>-->
-                  <!--</tr>-->
-                  <tr>
-                    <th>Sub Total</th>
-                    <td :key="ub.id" v-for="ub in budgets">GHS {{ub.asset_total}}</td>
-                    <td>ewrw</td>
-                  </tr>
-                  <tr>
-                    <th>Budget Total</th>
-                    <td :key="ub.id" v-for="ub in budgets">GHS {{ub.consolidated_basic_salary}}</td>
-                    <td>qeqqw</td>
-                  </tr>
+                    <!--</tr>-->
+                    <tr>
+                      <th>Sub Total</th>
+                      <td :key="ub.id" v-for="ub in budgets">GHS {{ub.asset_total}}</td>
+                      <td>ewrw</td>
+                    </tr>
+                    <tr>
+                      <th>Budget Total</th>
+                      <td :key="ub.id" v-for="ub in budgets">GHS {{ub.consolidated_basic_salary}}</td>
+                      <td>qeqqw</td>
+                    </tr>
                   </tbody>
-
                 </table>
               </div>
               <!--<div class="tab-content" id="myTabContent">-->
-                <!--<div class="tab-pane fade show active" id="division" role="tabpanel" aria-labelledby="division-tab">-->
-                  <!--Division Budget Summary-->
-                <!--</div>-->
-                <!--<div class="tab-pane fade" id="department" role="tabpanel" aria-labelledby="department-tab">-->
-                  <!--Department budget Summary-->
-                <!--</div>-->
-                <!--<div class="tab-pane fade" id="unit" role="tabpanel" aria-labelledby="unit-tab">-->
-                  <!--Department budget Summary-->
-                <!--</div>-->
+              <!--<div class="tab-pane fade show active" id="division" role="tabpanel" aria-labelledby="division-tab">-->
+              <!--Division Budget Summary-->
+              <!--</div>-->
+              <!--<div class="tab-pane fade" id="department" role="tabpanel" aria-labelledby="department-tab">-->
+              <!--Department budget Summary-->
+              <!--</div>-->
+              <!--<div class="tab-pane fade" id="unit" role="tabpanel" aria-labelledby="unit-tab">-->
+              <!--Department budget Summary-->
+              <!--</div>-->
               <!--</div>-->
             </div>
           </div>
@@ -145,42 +144,22 @@ export default {
   methods: {
     details() {
       const id = this.$route.params.period_id;
-      axios.get(`${config.apiUrl}/budget/bs/${id}/`, {
-        headers: {
-          Authorization: `JWT ${config.get_token()}`,
-        },
-      }).then((res) => {
-        const results = res.data;
-        this.object = results.object;
-        this.budgets = results.unit_budgets;
-        this.ubc = results.ubc + 1;
-      });
+      axios
+        .get(`${config.apiUrl}/budget/bs/${id}/`, {
+          headers: {
+            Authorization: `JWT ${config.get_token()}`,
+          },
+        })
+        .then((res) => {
+          const results = res.data;
+          this.object = results.object;
+          this.budgets = results.unit_budgets;
+          this.ubc = results.ubc + 1;
+        });
     },
-<<<<<<< HEAD
   },
   mounted() {
     this.details();
   },
 };
-=======
-    methods: {
-      details(){
-        const id = this.$route.params.period_id;
-        axios.get(`${config.apiUrl}/budget/bs/${id}/`, {
-          headers: {
-            Authorization: `JWT ${config.get_token()}`
-          }
-        }).then((res) => {
-          const results = res.data;
-          this.object = results.object;
-          this.budgets = results.unit_budgets;
-          this.ubc = results.ubc + 2;
-        })
-      }
-    },
-    mounted(){
-      this.details();
-    }
-  }
->>>>>>> 8c24d96b815e81906f9928fea9e7d018e9897b5a
 </script>
