5cb67ca9475cdc30ee93bb951194f923f47fe572
robertobirijnr <robertobirijnr@hotmail.com>
2020-07-30 04:14:32 +0000

    username to profile

--- src/components/Main/navbar.vue
+++ src/components/Main/navbar.vue
@@ -6,7 +6,7 @@
              <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
         <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-                <a class="nav-link" href="#"><h4 class="page-title">{{user.full_name}}</h4></a>
+                <router-link class="nav-link" to="/profile"><h4 class="page-title">{{user.full_name}}</h4></router-link>
             </li>
             <li class="nav-item">
                 <span class="nav-link" @click="logout()"><span class="material-icons">
